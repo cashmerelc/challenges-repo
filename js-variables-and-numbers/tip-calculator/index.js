55314a003e2011a53bbb1afc8874531a3da178a0
Derek Myers <cashmerelc@gmail.com>
2024-04-10 11:27:46 +0200

    Added js to calculate tip and total cost of meal.

---
+++ js-variables-and-numbers/tip-calculator/index.js
@@ -0,0 +1,7 @@
+let mealCost = 24.37;
+let tipPercentage = 15;
+
+let tipAmount = mealCost * (tipPercentage / 100);
+let totalCost = tipAmount + mealCost;
+
+console.log("The total cost of your bill is: " + totalCost + " euros");
