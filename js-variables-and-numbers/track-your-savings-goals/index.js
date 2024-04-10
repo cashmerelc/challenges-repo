aff5293a6a70b470bf49fd991f5ab5e75d9e9cc8
Derek Myers <cashmerelc@gmail.com>
2024-04-10 11:34:22 +0200

    Added script to calculate total savings

---
+++ js-variables-and-numbers/track-your-savings-goals/index.js
@@ -0,0 +1,13 @@
+let startingBalance = 1000;
+let monthlyContribution = 250;
+let months = 12;
+
+let totalSavings = startingBalance + monthlyContribution * months;
+
+console.log(
+  "After " +
+    months +
+    " months of saving, you will have $" +
+    totalSavings +
+    " in your account."
+);
