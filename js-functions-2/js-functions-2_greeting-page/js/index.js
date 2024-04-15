e4e062984ead049af56c06677cb9fbc5e2ea64d2
Derek Myers <cashmerelc@gmail.com>
2024-04-15 10:40:01 +0200

    Added greeting based on time of day and background color based on day of the week

--- js-functions-2/js-functions-2_greeting-page/js/index.js
+++ js-functions-2/js-functions-2_greeting-page/js/index.js
@@ -23,11 +23,41 @@ Change the contents of this page depending on the current day and time.
 const display = document.querySelector('[data-js="display"]');
 
 function getGreeting() {
-  // Code here
+  let currentTime = new Date().getHours();
+
+  if (currentTime >= 6 && currentTime <= 12) {
+    return "Good Morning";
+  }
+  if (currentTime >= 13 && currentTime <= 18) {
+    return "Good Afternoon";
+  }
+  if (currentTime >= 19 && currentTime <= 22) {
+    return "Good Evening";
+  }
+  if (currentTime >= 23 && currentTime <= 5) {
+    return "Good Night";
+  }
 }
 
 function getDayColor() {
-  // Code here
+  let currentDay = new Date().getDay();
+
+  switch (currentDay) {
+    case "monday":
+      return "darkgray";
+    case "tuesday":
+      return "lightblue";
+    case "wednesday":
+      return "lightblue";
+    case "thursday":
+      return "lightblue";
+    case "friday":
+      return "lightblue";
+    case "saturday":
+      return "hotpink";
+    case "sunday":
+      return "hotpink";
+  }
 }
 
 display.textContent = getGreeting();
