234ae5d2f5aaa5738a1da993a6703069668cedd4
Derek Myers <cashmerelc@gmail.com>
2024-04-17 16:11:23 +0200

    Add loop to change image source to display filled star based on the number passed into the renderStars function

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,17 +2,22 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
   for (let i = 1; i <= 5; i++) {
     const newStar = document.createElement("img");
-    newStar.src = "../assets/star-empty.svg";
-    starContainer.append(newStar);
+    if (i <= filledStars) {
+      newStar.src = "../assets/star-filled.svg";
+      starContainer.append(newStar);
+    } else {
+      newStar.src = "../assets/star-empty.svg";
+      starContainer.append(newStar);
+    }
   }
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(1);
