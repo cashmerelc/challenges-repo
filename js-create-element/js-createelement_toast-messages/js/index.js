48b1abfb6106a086c9b710d619a63fc026b5c920
Derek Myers <cashmerelc@gmail.com>
2024-04-17 11:13:58 +0200

    Create new li element and dd text and class on button click

--- js-create-element/js-createelement_toast-messages/js/index.js
+++ js-create-element/js-createelement_toast-messages/js/index.js
@@ -6,6 +6,10 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const newToast = document.createElement("li");
+  newToast.textContent = "I'm a toast message.";
+  newToast.classList.add("toast-container__message");
+  toastContainer.append(newToast);
 });
 
 clearButton.addEventListener("click", () => {
