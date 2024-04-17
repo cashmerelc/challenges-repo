57f15c633ff818b41eb4241f62410f00d89780ad
Derek Myers <cashmerelc@gmail.com>
2024-04-17 12:51:11 +0200

    Add second product to page using innerHTML, Find the name and price of newly-added product and print to console

--- js-create-element/js-createelement_product-teaser/js/index.js
+++ js-create-element/js-createelement_product-teaser/js/index.js
@@ -9,3 +9,55 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+// Create article element
+const newProduct = document.createElement("article");
+newProduct.classList.add("product");
+
+// Set the content of the article using innerHTML
+newProduct.innerHTML = `
+<div class="product__body">
+<div class="product__text-container">
+  <h2 class="product__name"> ${name} </h2>
+  <ul class="product__categories">
+            <li class="product__category">${category1}</li>
+            <li class="product__category">${category2}</li>
+            <li class="product__category">${category3}</li>
+          </ul>
+          <p class="product__description">
+            ${description}
+          </p>
+        </div>
+        <div class="product__image-container">
+          <img
+            class="product__image"
+            src="${imageSrc}"
+            alt=""
+          />
+        </div>
+      </div>
+      <footer class="product__footer">
+        <span class="product__price">${price}</span>
+        <button type="button" class="product__buy-button" data-js="queen-angelfish-buy-button">Buy</button>
+      </footer>
+`;
+
+// Append new product to the body
+document.body.append(newProduct);
+
+const angelfishBuyButton = document.querySelector(
+  '[data-js="queen-angelfish-buy-button"]'
+);
+
+angelfishBuyButton.addEventListener("click", () => {
+  // Get all product names and prices from the page
+  const productNames = document.querySelectorAll(".product__name");
+  const productPrices = document.querySelectorAll(".product__price");
+
+  // In this case we want to log the name and price of the second (last) product on the page
+
+  console.log(
+    productNames[productNames.length - 1].textContent,
+    productPrices[productPrices.length - 1].textContent
+  );
+});
