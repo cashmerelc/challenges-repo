35d430598acb1972ad8693fa41623c987244a2c1
Derek Myers <cashmerelc@gmail.com>
2024-04-17 15:10:54 +0200

    Add for of loop to iterate through the list of programming languages

--- js-loops/js-loops_forof-list/js/index.js
+++ js-loops/js-loops_forof-list/js/index.js
@@ -18,5 +18,9 @@ const programmingLanguages = [
 ];
 
 // --v-- write/change code here --v--
-
+for (const lang of programmingLanguages) {
+  const li = document.createElement("li");
+  li.textContent = programmingLanguages[lang];
+  main.ol.append(li);
+}
 // --^-- write/change code here --^--
