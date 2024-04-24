4d5f1115f82677d21c7fad73b9e5579e6d7e0d37
Derek Myers <cashmerelc@gmail.com>
2024-04-24 13:02:22 +0200

    Complete tasks until the ones requiring filters

--- js-array-methods-2/js-array-methods-2_animal-objects/index.js
+++ js-array-methods-2/js-array-methods-2_animal-objects/index.js
@@ -78,9 +78,13 @@ const animals = [
 ];
 
 // Hint: Besides the array method, check out the string method `startsWith()`.
-const firstAnimalStartingWithLetterG = null;
+const firstAnimalStartingWithLetterG = animals.find((animal) =>
+  animal.name.startsWith("g")
+);
 
-const indexOfAnimalWithNameLongerFive = null;
+const indexOfAnimalWithNameLongerFive = animals.findIndex(
+  (animal) => animal.name.length > 5
+);
 
 // Note:
 // - Sorting strings is slightly more complicated than sorting numbers.
@@ -91,16 +95,36 @@ const indexOfAnimalWithNameLongerFive = null;
 // Hint: sort() mutates the original array, which is bad.
 // -> Use animals.slice().sort(...) to make a copy (and the tests work).
 
-const animalsSortedAlphabetically = null;
+const animalsSortedAlphabetically = animals.sort(function (a, b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+});
 
-const animalsSortedByWeightStartingWithLowest = null;
+const animalsSortedByWeightStartingWithLowest = animals.sort(
+  (a, b) => a.weight - b.weight
+);
 
-const animalsSortedByWeightReversed = null;
+const animalsSortedByWeightReversed =
+  animalsSortedByWeightStartingWithLowest.reverse();
 
-const animalWithWeightMoreThanFivehundredExists = null;
+const animalWithWeightMoreThanFivehundredExists = animals.some(
+  (animal) => animal.weight > 500
+);
 
 // Hint: Filter for Europe first, then check every animal for its weight.
-const allAnimalsInEuropeWeighLessThanOnehundred = null;
+const europeAnimals = animals.filter((animal) => {
+  if (animal.continents.includes("europe")) {
+    return animal;
+  }
+});
+const allAnimalsInEuropeWeighLessThanOnehundred = europeAnimals.every(
+  (animal) => animal.weight < 100
+);
 
 // Hint: filter + map + reduce
 const weightOfAllAnimalsInAfrica = null;
