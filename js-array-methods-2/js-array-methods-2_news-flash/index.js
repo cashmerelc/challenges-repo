5af34c76949ddc2dd443669a192e295eb72c950f
Derek Myers <cashmerelc@gmail.com>
2024-04-24 16:34:36 +0200

    Complete news flash challenge

--- js-array-methods-2/js-array-methods-2_news-flash/index.js
+++ js-array-methods-2/js-array-methods-2_news-flash/index.js
@@ -5,12 +5,19 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
-});
-
+const filteredNews = news.filter((article) =>
+  article.categories.includes("politics")
+);
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews.sort((a, b) => {
+  if (a.body.length < b.body.length) {
+    return -1;
+  }
+  if (a.body.length > b.body.length) {
+    return 1;
+  }
+  return 0;
+});
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
