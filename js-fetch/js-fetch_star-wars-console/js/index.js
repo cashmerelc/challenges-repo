1b1a7842f5544c3fca45c084a56a8341c8cef15b
Derek Myers <cashmerelc@gmail.com>
2024-04-26 11:30:49 +0200

    Fetch Star Wars characters, log their names and R2-D2's eye color

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -1,12 +1,19 @@
 console.clear();
 
-const url = "https://swapi.dev/api/people";
+const url = "https://swapi.py4e.com/api/people";
 
 async function fetchData() {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    const characters = data.results;
+    console.log(characters);
+    characters.forEach((character) => {
+      console.log("Name: ", character.name);
+      if (character.name === "R2-D2") {
+        console.log("Eye Color: ", character["eye_color"]);
+      }
+    });
     return data;
   } catch (err) {
     console.log(err);
