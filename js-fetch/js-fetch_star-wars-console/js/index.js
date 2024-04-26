0de497ca4deda40ae9586524698569c88b9f0d69
Derek Myers <cashmerelc@gmail.com>
2024-04-26 11:06:48 +0200

    Implement fetch but receive cert date invalid error, possible issue with API

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,15 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 fetchData();
