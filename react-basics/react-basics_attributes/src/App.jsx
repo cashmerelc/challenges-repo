166e45c8181b595ef382b6ee6195b5f6d6fb6084
Derek Myers <cashmerelc@gmail.com>
2024-05-03 15:27:23 +0200

    Create article component and render

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">This is the article title</h2>
+      <label htmlFor="yourname">Your Name</label>
+      <input type="text" id="yourname" name="yourname"></input>
+      <a className="article__link" href="http://www.google.com">
+        Google
+      </a>
+    </article>
+  );
 }
