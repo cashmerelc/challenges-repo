5bae335551b93e1895e32de525c583c324adc1b3
Derek Myers <cashmerelc@gmail.com>
2024-05-06 11:05:08 +0200

    Add onClick to button

--- react-props/react-props_button/src/App.jsx
+++ react-props/react-props_button/src/App.jsx
@@ -6,7 +6,11 @@ export default function App() {
 
 function Button({ color, disabled, text }) {
   return (
-    <button style={{ color: color }} disabled={disabled}>
+    <button
+      onClick={() => alert("You clicked!")}
+      style={{ color: color }}
+      disabled={disabled}
+    >
       {text}
     </button>
   );
