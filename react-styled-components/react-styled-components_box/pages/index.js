9646926a6c08a420a6e5682a35b569e07228c907
Derek Myers <cashmerelc@gmail.com>
2024-05-21 15:42:04 +0200

    Align boxes horizontally

--- react-styled-components/react-styled-components_box/pages/index.js
+++ react-styled-components/react-styled-components_box/pages/index.js
@@ -1,13 +1,20 @@
 import { BoxWithStyledComponents } from "@/components/BoxWithStyledComponents/BoxWithStyledComponents.js";
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import styled from "styled-components";
 
 export default function HomePage() {
   return (
-    <div>
+    <HorizontalDiv>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
       <BoxWithStyledComponents />
       <BoxWithStyledComponents $isBlack />
-    </div>
+    </HorizontalDiv>
   );
 }
+
+const HorizontalDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
